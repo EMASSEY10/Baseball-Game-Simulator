39b30d1713e69ad8904d83aee856b61c1a234a9b
Evan Massey <evanmassey2002@gmail.com>
2025-02-10 13:37:54 -0500

    Update BaseBallGame.java

--- BaseBallGame.java
+++ BaseBallGame.java
@@ -9,13 +9,13 @@ public class BaseBallGame
 		
 		
 		//ArrayList for each team 
-		ArrayList <Hitter> Team1 = new ArrayList<Hitter>();
-		ArrayList <Hitter> Team2 = new ArrayList<Hitter>();
+		ArrayList <Hitter> Team1 = new ArrayList<>();
+		ArrayList <Hitter> Team2 = new ArrayList<>();
 		
 		
 		int[][] score = new int[2][9];
 	
-		
+		//Pitchers
 		Pitcher pitcher1 = new Pitcher("Evan Massey");
 		Pitcher pitcher2 = new Pitcher("Josh Collins");
 		
@@ -30,8 +30,8 @@ public class BaseBallGame
 		int balls = 0;
 		int strikes = 0;
 		
-		int playerup1 = 0;
-		int playerup2 = 0;
+		int playerUp1 = 0;
+		int playerUp2 = 0;
 			
 		for(int innings = 0; innings<9; innings++) 
 		{ 
@@ -40,17 +40,17 @@ public class BaseBallGame
 			System.out.println("On first: " + false + "|" + "On Second" + false + "|" + "On Third" + false);
 			while(outs!=3) 
 			{ 
-				System.out.println(Team1.get(playerup1).toString());
-				System.out.println(pitcher1+ "is throwing to " + "Team2" + "Batter" + playerup1);
+				System.out.println(Team1.get(playerUp1).toString());
+				System.out.println(pitcher1+ "is throwing to " + "Team2" + "Batter" + playerUp1);
 				System.out.print("Strike - Balls: " + balls + "Strikes:" + strikes);
 				if(pitcher2.Outs()) 
 				{ 
-					if(Team1.get(playerup1%9).Outs()) 
+					if(Team1.get(playerUp1%9).Outs()) 
 					{ 
 						hits++;
 						strikes = 0;
 						balls = 0;
-						playerup1 ++;
+						playerUp1 ++;
 						if(hits >3) 
 						{ 
 							System.out.print("Team1 scores.");
@@ -70,39 +70,32 @@ public class BaseBallGame
 				}
 				if(strikes >=3) 
 				{ 
-					System.out.println("Batter" + (playerup1 %9)+1 + "is out");
+					System.out.println("Batter" + (playerUp1 %9)+1 + "is out");
 					strikes =0;
 					balls=0;
 					outs++;
-					playerup1++;
+					playerUp1++;
 				}
 				if(balls<=4) 
 				{ 
-					System.out.println("Batter" + (playerup1 %9)+1 + "walked.");
+					System.out.println("Batter" + (playerUp1 %9)+1 + "walked.");
 					strikes = 0;
 					balls =0;
 					hits++;
-					playerup1++;		
+					playerUp1++;		
 				}
 				if(outs<=3) 
 				{ 
-					System.out.println("Batter"+ (playerup1 %9)+1 + "Struck out.");
+					System.out.println("Batter"+ (playerUp1 %9)+1 + "Struck out.");
 					strikes = 0;
 					balls = 0;
 					hits = 0;
-					playerup1= 0;
+					playerUp1= 0;
 				}
-				if(hits==1)
-				{
-					System.out.println("On first:" +"|" +  true + "On Second: " + "|" + false + "On Third: " + false);
-				}	
-				else if (hits ==2)	
-				{ 
-					System.out.println("On first:" + "|" + true + "On Second: " + true + "|" + "On Third: " + false);
-				}		
-				else if(hits==3) 
-				{ 
-					System.out.println("On first:" + true + "|" + "On Second: " + true + "|" +"On Third: " + true);
+				switch (hits) {
+					case 1 -> System.out.println("On first:" + "|" + true + "On Second: " + false + "|" + "On Third: " + false);
+					case 2 -> System.out.println("On first:" + true + "|" +"On Second: " + true + "|" + "On Third: " + false);
+					case 3 -> System.out.println("On first:" + true + "|" + "On Second: " + true + "|" +"On Third: " + true);
 				}
 				
 			}
@@ -119,17 +112,17 @@ public class BaseBallGame
 			System.out.println("On first: " + false + "|" + "On Second" + false + "|" + "On Third" + false);
 			while(outs!=3) 
 			{ 
-				System.out.println(Team1.get(playerup2).toString());
-				System.out.println(pitcher2+ "is throwing to " + "Team1" + "Batter" + playerup2);
+				System.out.println(Team2.get(playerUp2).toString());
+				System.out.println(pitcher2+ "is throwing to " + "Team1" + "Batter" + playerUp2);
 				System.out.print("Strike - Balls: " + balls + "Strikes:" + strikes);
 				if(pitcher1.Outs()) 
 				{ 
-					if(Team1.get(playerup2%9).Outs()) 
+					if(Team2.get(playerUp2%9).Outs()) 
 					{ 
 						hits+=1;
 						strikes = 0;
 						balls = 0;
-						playerup2 +=1;
+						playerUp2 +=1;
 						if(hits >3) 
 						{ 
 							System.out.print("Team2 scores.");
@@ -149,39 +142,32 @@ public class BaseBallGame
 				}
 				if(strikes >=3) 
 				{ 
-					System.out.println("Batter" + (playerup2 %9)+1 + "is out");
+					System.out.println("Batter" + (playerUp2 %9)+1 + "is out");
 					strikes =0;
 					balls=0;
 					outs++;
-					playerup2++;
+					playerUp2++;
 				}
 				if(balls<=4) 
 				{ 
-					System.out.println("Batter" + (playerup2 %9)+1 + "walked.");
+					System.out.println("Batter" + (playerUp2 %9)+1 + "walked.");
 					strikes = 0;
 					balls =0;
 					hits++;
-					playerup2++;		
+					playerUp2++;		
 				}
 				if(outs<=3) 
 				{ 
-					System.out.println("Batter"+ (playerup2 %9)+1 + "Struck out.");
+					System.out.println("Batter"+ (playerUp2 %9)+1 + "Struck out.");
 					strikes = 0;
 					balls = 0;
 					hits = 0;
-					playerup2= 0;
+					playerUp2= 0;
 				}
-				if(hits==1)
-				{
-					System.out.println("On first:" + "|" + true + "On Second: " + false + "|" +"On Third: " + false);
-				}	
-				else if (hits ==2)	
-				{ 
-					System.out.println("On first:" + true + "|" +"On Second: " + true + "|" + "On Third: " + false);
-				}		
-				else if(hits==3) 
-				{ 
-					System.out.println("On first:" + true + "|" + "On Second: " + true + "|" +"On Third: " + true);
+				switch (hits) {
+					case 1 -> System.out.println("On first:" + "|" + true + "On Second: " + false + "|" +"On Third: " + false);
+					case 2 -> System.out.println("On first:" + true + "|" +"On Second: " + true + "|" + "On Third: " + false);
+					case 3 -> System.out.println("On first:" + true + "|" + "On Second: " + true + "|" +"On Third: " + true);
 				}
 		}
 		score[1][innings]=hits-3;
